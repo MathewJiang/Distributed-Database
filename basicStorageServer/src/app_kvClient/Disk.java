8406dd4a80616ab7aa643677fbf41f23b824a396
WyattLiu <wyatt_liu@hotmail.com>
2019-01-17 15:46:55 -0500

    add test

---
+++ basicStorageServer/src/app_kvClient/Disk.java
@@ -0,0 +1,107 @@
+package app_kvClient;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.FileReader;
+
+
+public class Disk {
+	static String path = "";
+	static String db_dir = "";
+	public static void echo(String line) {
+		System.out.println(line);
+	}
+	
+	public static void test_and_set_db() {
+		// mkdir at local dir if not being created
+		db_dir = path + "/kvdb";
+		File db = new File(db_dir);
+		if(db.exists()) {
+			echo("db exists");
+		} else {
+			echo("db does not exist, making a new db");
+			if(db.mkdir()) {
+				echo("Mkdir sucess");
+			} else {
+				echo("Mkdir failed");
+			}
+		}
+		// this point, we have a db dir initialized
+	}
+	
+	public static String getKV(String key) throws Exception {
+		
+		String src = db_dir+"/"+key;
+		File search = new File(src);
+		if(search.exists()) {
+			// we have this key
+			
+		} else {
+			// we don't have this key
+			throw new Exception();
+		}
+		
+		FileReader fr = new FileReader(src);
+		BufferedReader fh = new BufferedReader(fr);
+		
+		String result = "";
+		String line;
+		while((line = fh.readLine()) != null){
+			result += line;
+		}
+		return result;
+	}
+	
+	public static void putKV(String key, String value){
+		String dest = db_dir+"/"+key;
+		File search = new File(dest);
+		if(search.exists()) {
+			// we have this key, put update the value
+			
+		} else {
+			// we don't have this key, put add the file
+			try {
+				search.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		try {
+			PrintWriter key_file = new PrintWriter(dest);
+			key_file.println(value);
+			key_file.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return;
+	}
+	
+	public static void clearStorage(){
+		db_dir = path + "/kvdb";
+		File db = new File(db_dir);
+		
+		String[] entries = db.list();
+		//echo("Removing files under db_dir");
+		for(int i = 0; i < entries.length; i++){
+		    File curr = new File(entries[i]);
+		    curr.delete();
+		}
+		//echo("Removed files under db_dir");
+	}
+
+	public static void init() {
+		path = System.getProperty("user.dir");
+		String pwd_debug_info = "Working directory is " + path;
+		echo(pwd_debug_info);
+		test_and_set_db();
+		
+		
+	}
+
+}
