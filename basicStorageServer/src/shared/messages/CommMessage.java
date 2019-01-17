ba301ca8e08c54844231307f588dbbe651479ce5
linyanna <zzlyn321@hotmail.com>
2019-01-17 12:41:35 -0500

    simplify message communications

--- basicStorageServer/src/shared/messages/CommMessage.java
+++ basicStorageServer/src/shared/messages/CommMessage.java
@@ -4,8 +4,8 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
 
-public class CommMessage {
-	// CONNECT/DISCONNECT requests should come with an empty message field.
+public class CommMessage implements KVMessage {
+	
 	public enum OptCode {
 		PUT, /* Put - key value storage */
 		GET, /* Get - key value storage */
@@ -13,7 +13,36 @@ public class CommMessage {
 	
 	public OptCode opt;
 	
-	public RequestKVMessage message;
+	private StatusType status;
+	private String key;
+	private String value;
+
+	@Override
+	public String getKey() {
+		return this.key;
+	}
+
+	@Override
+	public String getValue() {
+		return this.value;
+	}
+
+	@Override
+	public StatusType getStatus() {
+		return this.status;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public void setStatus(StatusType status) {
+		this.status = status;
+	}
 	
 	// Serialize a CommMessage into a byte array that represents a JSON string.
 	public static byte[] serialize(CommMessage msg) {
@@ -21,8 +50,8 @@ public class CommMessage {
 		return gson.toJson(msg).getBytes();
 	}
 	
-	// Deserialize a CommMessage from given byte array data. Throws JsonSyntaxException
-	// if the JSON string embedded in data is not of the right format.
+	// Deserialize a CommMessage from given byte array data. Throws Exception
+	// if the JSON string embedded in data is not of the correct format.
 	public static CommMessage deserialize(byte[] data) throws JsonSyntaxException{
 		Gson gson = new GsonBuilder().serializeNulls().create();
 		return gson.fromJson(new String(data), CommMessage.class);
