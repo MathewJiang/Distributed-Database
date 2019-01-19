1285b571f0155cbd890af35b8ecce19e4d04b1e1
linyanna <zzlyn321@hotmail.com>
2019-01-19 15:41:34 -0500

    Moved Client logic to KVStore. Only supports PUT operations now.

--- basicStorageServer/src/shared/CommMessageBuilder.java
+++ basicStorageServer/src/shared/CommMessageBuilder.java
@@ -6,11 +6,6 @@ import shared.messages.KVMessage;
 public class CommMessageBuilder {
 	private CommMessage msg = new CommMessage();
 	
-	public CommMessageBuilder setOpt(CommMessage.OptCode opt) {
-		msg.opt = opt;
-		return this;
-	}
-	
 	public CommMessageBuilder setStatus(KVMessage.StatusType status) {
 		msg.setStatus(status);
 		return this;
