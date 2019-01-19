c9dcb4e4fa0767a72a226bec540d35b7c66cc807
zzlyn <zzlyn321@hotmail.com>
2019-01-19 16:53:41 -0500

    test1

--- basicStorageServer/src/testing/StorageTest.java
+++ basicStorageServer/src/testing/StorageTest.java
@@ -18,15 +18,14 @@ public class StorageTest {
 	public void test() {
 		fail("Not yet implemented");
 	}
-	
-	
+
 	static String path = "";
-	
+
 	public void testDiskPerf_config() {
-		File test_log = new File(path+"perf.log");
-		if(test_log.exists()) {
+		File test_log = new File(path + "perf.log");
+		if (test_log.exists()) {
 			test_log.delete();
-			
+
 		}
 		try {
 			test_log.createNewFile();
@@ -34,25 +33,25 @@ public class StorageTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		int i = 100;
-		while(i < 1000) {
+		while (i < 1000) {
 			testDiskPerf(i);
-			i+=100;
+			i += 100;
 		}
 	}
-	
+
 	public long testDiskPerf(int num_files) {
-    	Disk.init(); // must init first
-    	Disk.clearStorage();
-		
-    	final long startTime = System.currentTimeMillis();
-    	Disk.putKV("a", "1209");
-    	for(int i = 0; i < num_files; i++) {
-    		Disk.putKV(((Integer)i).toString(), ((Integer)i).toString());
-    	}
-    	
-    	try {
+		Disk.init(); // must init first
+		Disk.clearStorage();
+
+		final long startTime = System.currentTimeMillis();
+		Disk.putKV("a", "1209");
+		for (int i = 0; i < num_files; i++) {
+			Disk.putKV(((Integer) i).toString(), ((Integer) i).toString());
+		}
+
+		try {
 			String read = Disk.getKV("a");
 			Disk.echo(read);
 			read = Disk.getKV("8475");
@@ -61,49 +60,52 @@ public class StorageTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	final long endTime = System.currentTimeMillis();
-    	String perfTime = "num_files = " + num_files + " Total used " + (endTime - startTime) + "\n";
-    	append(path+"perf.log", perfTime);
-    	return (endTime - startTime);
+		final long endTime = System.currentTimeMillis();
+		String perfTime = "num_files = " + num_files + " Total used "
+				+ (endTime - startTime) + "\n";
+		append(path + "perf.log", perfTime);
+		return (endTime - startTime);
 	}
-	
+
 	public static void testFuncFifoCache() {
 		Disk.init();
 		Disk.clearStorage();
 		fifoCache.set_cache_size(5);
-		fifoCache.putKV("iPhone","XS");
-		fifoCache.putKV("Apple","iPad");
-		for(int i =0; i<10;i++) {
-			fifoCache.putKV(((Integer)i).toString(), ((Integer)(i+1)).toString());
+		fifoCache.putKV("iPhone", "XS");
+		fifoCache.putKV("Apple", "iPad");
+		for (int i = 0; i < 10; i++) {
+			fifoCache.putKV(((Integer) i).toString(),
+					((Integer) (i + 1)).toString());
 		}
-		fifoCache.putKV("ECE344","DEADBEEF");
-		fifoCache.putKV("ECE454","GOL");
+		fifoCache.putKV("ECE344", "DEADBEEF");
+		fifoCache.putKV("ECE454", "GOL");
 		String test;
 		try {
 			test = fifoCache.getKV("Apple");
-			if(test == "Apple") {
+			if (test == "Apple") {
 				Disk.echo("Key is not lost");
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		try {
 			test = Disk.getKV("Apple");
-			if(test.equals("iPad")) {
+			if (test.equals("iPad")) {
 				Disk.echo("Pass, early keys should be in Disk");
 			} else {
-				Disk.echo("Fail, early keys should be in Disk and I got " + test);
+				Disk.echo("Fail, early keys should be in Disk and I got "
+						+ test);
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		try {
 			test = Disk.getKV("ECE454");
-			if(test.equals("GOL")) {
+			if (test.equals("GOL")) {
 				Disk.echo("Fail, late keys should not be in Disk");
 			} else {
 				Disk.echo("Pass, late keys should not be in Disk");
@@ -111,10 +113,10 @@ public class StorageTest {
 		} catch (Exception e) {
 			Disk.echo("Expected");
 		}
-		Disk.echo("disk has "+Disk.key_count()+" entires");
+		Disk.echo("disk has " + Disk.key_count() + " entires");
 		fifoCache.flush_to_disk();
 	}
-	
+
 	public static void append(String file_path, String content) {
 		OutputStream os;
 		try {
@@ -125,8 +127,7 @@ public class StorageTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	}
 
+	}
 
 }
