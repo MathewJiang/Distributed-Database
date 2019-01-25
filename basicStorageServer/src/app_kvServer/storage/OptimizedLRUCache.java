c186b28bb1af8e3893d3f461f3e8cf48d10117fb
zzlyn <zzlyn321@hotmail.com>
2019-01-25 14:04:51 -0500

    Moved linkedentries list into LRU.

--- basicStorageServer/src/app_kvServer/storage/OptimizedLRUCache.java
+++ basicStorageServer/src/app_kvServer/storage/OptimizedLRUCache.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
-import app_kvServer.storage.LinkedEntries.EntryNode;
+
+import app_kvServer.storage.OptimizedLRUCache.LinkedEntries.EntryNode;
 
 import shared.messages.KVMessage.StatusType;
 
@@ -104,5 +105,68 @@ public class OptimizedLRUCache {
 			it.remove();
 		}
 		list.clear();
+	}
+	
+	// TODO: Un-static this class after storage refactoring.
+	public static class LinkedEntries {
+		
+		// Node of a double linked list.
+		public class EntryNode {
+			String key;
+			String value;
+			EntryNode prev;
+			EntryNode next;
+
+			public EntryNode() {
+			}
+
+			public EntryNode(String k, String v) {
+				key = k;
+				value = v;
+			}
+		}
+
+		private EntryNode head;
+		
+		private EntryNode tail;
+
+		public LinkedEntries() {
+			head = new EntryNode();
+			tail = new EntryNode();
+			head.next = tail;
+			tail.prev = head;
+		}
+
+		public EntryNode getHead() {
+			return head.next;
+		}
+
+		public void addHead(EntryNode node) {
+			node.prev = head;
+			node.next = head.next;
+			head.next.prev = node;
+			head.next = node;
+		}
+
+		public EntryNode getTail() {
+			return tail.prev;
+		}
+
+		public void addTail(EntryNode node) {
+			node.prev = tail.prev;
+			node.next = tail;
+			tail.prev.next = node;
+			tail.prev = node;
+		}
+
+		public void remove(EntryNode node) {
+			node.prev.next = node.next;
+			node.next.prev = node.prev;
+		}
+
+		public void clear() {
+			head.next = tail;
+			tail.prev = head;
+		}
 	}
 }
