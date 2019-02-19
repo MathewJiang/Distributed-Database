a94a0037699e19f7e00da5aabe759e9487f282f0
Jiang  Zheping <jiangz32@ug163.eecg.toronto.edu>
2019-02-19 16:21:42 -0500

    comment out more m1 tests

--- basicStorageServer/src/testing/ConsistentHashTest.java
+++ basicStorageServer/src/testing/ConsistentHashTest.java
@@ -5,14 +5,32 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ConsistentHashTest {
+import shared.ConsistentHash;
+import shared.InfraMetadata.ServiceLocation;
 
+public class ConsistentHashTest {
+	ConsistentHash ch;
+	
+	ServiceLocation server0;
+	ServiceLocation server1;
+	ServiceLocation server2;
+	ServiceLocation server3;
+	ServiceLocation server4;
+	ServiceLocation server5;
+	
 	@Before
 	public void setUp() throws Exception {
+		server0 = new ServiceLocation("server0", "127.0.0.1", 50422);
+		server1 = new ServiceLocation("server1", "127.0.0.1", 50000);
+		server2 = new ServiceLocation("server2", "127.0.0.1", 50007);
+		server3 = new ServiceLocation("server3", "127.0.0.1", 50001);
+		server4 = new ServiceLocation("server4", "127.0.0.1", 50006);
+		server5 = new ServiceLocation("server5", "127.0.0.1", 50004);
 	}
 
 	@Test
 	public void testHashRingConstruct() {
+		ch = new ConsistentHash();
 		fail("Not yet implemented");
 	}
 
