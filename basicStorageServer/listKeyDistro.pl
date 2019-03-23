5c2d35090d870a0f10e75cd5091f7ce3a1c2dd3a
WyattLiu <wyatt_liu@hotmail.com>
2019-03-23 13:50:20 -0400

    update script

--- basicStorageServer/listKeyDistro.pl
+++ basicStorageServer/listKeyDistro.pl
@@ -6,6 +6,7 @@ use warnings;
 my @servers = `readlink -f ./kvdb/*`;
 
 my $sum = 0;
+my $replica_sum = 0;
 foreach my $server(@servers) {
 	chomp $server;
 	
@@ -15,8 +16,12 @@ foreach my $server(@servers) {
 		chomp $key_count;
 		if($server !~ /.*replica.*/) {
 			$sum += $key_count;
+		} else {
+			$replica_sum += $key_count;
 		}
+		
 	}
 }
 
 print "total key count: $sum\n";
+print "toal replica key count: $replica_sum\n";
